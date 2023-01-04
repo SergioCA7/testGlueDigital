494247dbe3616c9d21453513b47eabc0a87c16f3
SergioCA7 <sergio_cuenca_7@hotmail.com>
2023-01-04 14:17:29 +0100

    create project

---
+++ testGlueDigital/src/main/java/com/sergiocuenca/pruebagluedigital/PruebagluedigitalApplication.java
@@ -0,0 +1,13 @@
+package com.sergiocuenca.pruebagluedigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebagluedigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebagluedigitalApplication.class, args);
+	}
+
+}
---
+++ testGlueDigital/src/test/java/com/sergiocuenca/pruebagluedigital/PruebagluedigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiocuenca.pruebagluedigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebagluedigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
